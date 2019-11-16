479867affc33a44d01ff8678b018833b8a4aedda
liupeidong5 <handhand>
2019-11-16 22:34:05 +0800

    Execute the eureka-client successfully. springboot 1.5.3 eurekaServer 1.3.0 eurekaClient 1.3.0

--- eureka-client/src/main/java/com/liupeidong/spring/eurekaCli/EurekaCliApplication.java
+++ eureka-client/src/main/java/com/liupeidong/spring/eurekaCli/EurekaCliApplication.java
@@ -3,14 +3,15 @@ package com.liupeidong.spring.eurekaCli;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @RestController
-@EnableEurekaClient
 public class EurekaCliApplication {
     @Value("${server.port}")
     String port;
@@ -22,7 +23,7 @@ public class EurekaCliApplication {
     }
 
     @RequestMapping("/hello")
-    public String home(@RequestParam(value = "name", defaultValue = "adou") String name) {
+    public String home(@RequestParam(value = "name", defaultValue = "marus") String name) {
         return "hello " + name + " ,i am from port:" + port + "\n" + eurekaUrl;
     }
 }
