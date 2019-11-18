1e13a786486f7b1f1a648e513bc1aa0e7c2df5da
liupeidong5 <handhand>
2019-11-19 00:07:24 +0800

    update configs/application-*.yml update configserver/src/ConfigServerApplication.java update configserver/application.yml

--- configserver/src/main/java/com/liupeidong/spring/git/ConfigServerApplication.java
+++ configserver/src/main/java/com/liupeidong/spring/git/ConfigServerApplication.java
@@ -1,16 +1,13 @@
 package com.liupeidong.spring.git;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-/**
- * Created by fp295 on 2018/6/29.
- */
+@SpringBootApplication
+@EnableDiscoveryClient
 @EnableConfigServer
-@SpringCloudApplication
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
