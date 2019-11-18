80d3ae722253aad11692666d26ddce254eeb1bbc
liupeidong5 <handhand>
2019-11-18 17:08:24 +0800

    Add the configs & configserver without test

---
+++ configserver/src/main/java/com/liupeidong/spring/git/ConfigServerApplication.java
@@ -0,0 +1,19 @@
+package com.liupeidong.spring.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Created by fp295 on 2018/6/29.
+ */
+@EnableConfigServer
+@SpringCloudApplication
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
